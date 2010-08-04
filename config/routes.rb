a90bf028096ce5cd217d2c2c697f8c849bf4ce88
miros <mirosm@mirosm.ru>
2010-08-04 16:00:23 +0400

    root controller was set to cinemas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :cinemas
-
+  map.root :controller => :cinemas
 
   # The priority is based upon order of creation: first created -> highest priority.
 
