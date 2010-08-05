1232fb8d4004623e396eeff486642bd96313c1a6
miros <mirosm@mirosm.ru>
2010-08-05 12:55:45 +0400

    Rspec test for cinemas controller

---
+++ spec/controllers/cinemas_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe CinemasController do
+
+  integrate_views
+
+  describe "GET 'cinemas'" do
+    it "should be successful" do
+
+      cinema = mock_model(Cinema, :id => 1, :name => 'test_cinema_name')
+      Cinema.should_receive(:find).with(:all, {:order=>"name ASC"}).and_return([cinema])
+
+      get 'index'
+      response.should be_success
+      response.should include_text '1'
+      response.should include_text 'test_cinema_name'
+
+    end
+  end
+
+  describe "GET 'cinemas/1'" do
+    it "should be successful" do
+
+      cinema = mock_model(Cinema, :id => 1, :name => 'test_cinema_name', :city => 'москва')
+      show = mock_model(Show, :time => '12:00')
+      movie = mock_model(Movie, :name => 'test_movie_name')
+
+      cinema.should_receive(:shows).and_return([show])
+      show.should_receive(:movie).and_return(movie)
+
+      Cinema.should_receive(:find).with('1').and_return(cinema)
+
+      get 'show', {:id => '1'}
+
+      response.should be_success
+
+      response.should include_text '1'
+      response.should include_text 'test_cinema_name'
+      response.should include_text 'москва'
+      response.should include_text 'test_movie_name'
+      response.should include_text '12:00'
+
+    end
+  end
+
+
+end
