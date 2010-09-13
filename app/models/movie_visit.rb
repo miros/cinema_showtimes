150809b6eda981ff5fc4fb24a68d1ed3bceb115a
miros <mirosm@mirosm.ru>
2010-09-13 16:17:09 +0400

    little improvements in visited movies interface

--- app/models/movie_visit.rb
+++ app/models/movie_visit.rb
@@ -1,11 +1,11 @@
 class MovieVisit < ActiveRecord::Base
 
-  default_scope :order => 'date DESC'
+  default_scope :joins => :movie, :order => 'date DESC, movies.name ASC'
 
   belongs_to :user
   belongs_to :cinema
   belongs_to :movie
 
-  validates_presence_of :user_id, :movie_id, :cinema_id
+  validates_presence_of :user_id, :movie_id
 
 end
