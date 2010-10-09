12efd7e7c993a46fa8f510931c4d433ec5e69176
miros <mirosm@mirosm.ru>
2010-10-09 17:14:44 +0400

    Newrelic rpm gem installed

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'nokogiri'
   config.gem 'mechanize'
+  config.gem 'newrelic_rpm'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
